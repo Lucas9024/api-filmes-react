e6cfc76c02385f589cfecd8f827b2bc5f05063cc
Lucas9024 <lucassfrs9080@gmail.com>
2023-01-15 23:57:51 -0300

    Subindo interface basica pronta

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Container, List } from './styles';
+import { Container, Li, List} from './styles';
 
 function Home() {
 
+
+    {/*Criando array para listar os animes*/}
     const animes = [
         
         {
             title: 'Naruto Shipuden',
-            image_url: 'https://m.media-amazon.com/images/I/815ojmQiBgL._AC_SX466_.jpg'
+            image_url: 'https://cf.geekdo-images.com/gf25Q2fun2X5WnjwmQIOvA__opengraph/img/mgvE1sMniljGIgXK9caUTJobpWA=/fit-in/1200x630/filters:strip_icc()/pic2928408.jpg'
         },
         {
             title: 'Bleach',
@@ -19,7 +21,7 @@ function Home() {
         },
         {
             title: 'Yu Yu Hakusho',
-            image_url: 'https://www.crunchyroll.com/imgsrv/display/thumbnail/480x720/catalog/crunchyroll/e60626c56bf7437857e0caf7cbc4bf4a.jpe'
+            image_url: 'https://www.justwatch.com/images/poster/250677199/s332/temporada-4'
         },
         {
             title: 'Rainbow',
@@ -28,36 +30,56 @@ function Home() {
 
         {
             title: 'One Piece',
-            img_url: 'https://www.crunchyroll.com/imgsrv/display/thumbnail/480x720/catalog/crunchyroll/2239c7b46b2e491ae33b33ff980e9fb1.jpe'
+            image_url: 'https://www.crunchyroll.com/imgsrv/display/thumbnail/480x720/catalog/crunchyroll/2239c7b46b2e491ae33b33ff980e9fb1.jpe'
         }
     ];
+    {/* Criando array para listar os meus filmes */}
+
+    const movies = [
+        {
+            title: "Matrix Revolutions",
+            img_url: "https://upload.wikimedia.org/wikipedia/pt/9/94/Matrix_revolutions.jpg",
+
+        },
+
+        {
+            title: "Blade Runner 2049",
+            img_url: "https://upload.wikimedia.org/wikipedia/pt/a/af/Blade_Runner_2049.png"
+        }
+    ]
 
     return (
 
-       
-
         <Container>
             
-        <h1>Aqui esta a lista dos seus melhores filmes!</h1>
+        <h1>Aqui esta a lista dos melhores filmes e animes disponíveis para você!</h1>
 
         <List>
         {animes.map(anime => {
-            return (
-                <ul>
-                    <a href='https://google.com'>
-                    <li>
-                        
-                    <img src={anime.image_url}/>
-                    <h2>{anime.title}</h2>
-
-                    </li>
-                    </a>
-                   
-                </ul>
+            return (   
+                    <Li>
+                      <a href='https://google.com'>
+                      <img src={anime.image_url}/>
+                        <h2>{anime.title}</h2>
+                        </a>  
+                    </Li>
             );
         })}
+
+        {movies.map (movie => {
+            return (
+                
+                    <Li>
+                        <a href='https://google.com'>
+                        <img src={movie.img_url}/>
+                        <h2>{movie.title}</h2>
+                        </a>
+                    </Li>
+                
+            );
+        })}
+
         </List>
-            
         </Container>
     );
 }
--- src/Pages/Home/styles.js
+++ src/Pages/Home/styles.js
@@ -2,13 +2,42 @@ import styled from "styled-components";
 
 
 export const Container = styled.div`
-
+    h1 {
+        text-align: center;
+        padding: 4rem 0;
+}
 `;
 
 export const List = styled.ul`
-
+    text-align: center;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+    column-gap: 50px;
+    row-gap: 20px;
 `;
+
+
+
 
 export const Li = styled.li`
+     display: flex;
+     flex-direction: column;
+     justify-content: space-around;
 
-`;
+     img {
+        width: 300px;
+        object-fit: cover;
+     }
+     h2{
+        text-align: center;
+     }
+
+     a {
+    list-style: none;
+    text-decoration: none;
+    color: #f5f5f5;
+    padding: 2rem;
+     }
+
+    `;
+
